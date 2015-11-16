bea4cc731e70ef11818f243d883aaa238b81739a
goodfaiter <goodfaiter@hotmail.com>
2015-11-16 18:40:50 +0000

    Update

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,8 +8,12 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_accessor :word, :guesses, :wrong_guesses
+  
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
   end
 
   def self.get_random_word
@@ -17,6 +21,16 @@ class HangpersonGame
     require 'net/http'
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
+  end
+
+  def guess(letters)
+    if !@guesses.include?(letters) && !@wrong_guesses.include?(letters)
+      @guesses = letters if @word.include?(letters)
+      @wrong_guesses = letters if !@word.include?(letters)
+    else
+      puts "Letter already used!"
+      false
+    end
   end
 
 end
