450e8d83a3e8c93707a30951a705e3d4a32af4e9
goodfaiter <goodfaiter@hotmail.com>
2016-06-20 22:02:19 +0000

    Solved

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,7 +7,7 @@ class HangpersonGame
 
   # def initialize()
   # end
-  
+  #
   attr_accessor :word, :guesses, :wrong_guesses
   
   def initialize(word)
@@ -24,14 +24,45 @@ class HangpersonGame
   end
 
   def guess(letters)
-    false if letters.include?(/[A-Z]/)
-    if !@guesses.include?(letters) && !@wrong_guesses.include?(letters)
-      @guesses = letters if @word.include?(letters)
-      @wrong_guesses = letters if !@word.include?(letters)
+    raise ArgumentError if letters == nil
+    letters.downcase!
+    raise ArgumentError if letters.empty? || !(letters =~ /[a-z]/)
+    
+    if @guesses.include?(letters) || @wrong_guesses.include?(letters)
+      return false
     else
-      puts "Letter already used!"
-      false
+      @guesses << letters if @word.include?(letters)
+      @wrong_guesses << letters if !@word.include?(letters)
     end
   end
-
+  
+  def word_with_guesses ()
+    disp_word = ''
+    @word.each_char do |w|
+      if @guesses.include?(w)
+        disp_word << w
+      else
+        disp_word << '-'
+      end
+    end
+    return disp_word
+  end
+  
+  def check_win_or_lose()
+    check = true
+    @word.each_char do |w|
+      if !@guesses.include?(w)
+        check = false
+      end
+    end
+    if check == true
+      return :win
+    elsif (@guesses.length+@wrong_guesses.length) < 7 && check == false
+      return :play
+    else
+      return :lose
+    end
+  end
+  
 end
+
